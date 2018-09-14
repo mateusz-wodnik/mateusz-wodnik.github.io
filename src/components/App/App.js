36b72bff64a719a10835458228a50e993ae2f01d
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-09-14 21:36:00 +0200

    Add particles background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import { hot } from "react-hot-loader";
 import {StoreConsumer} from '../../Store';
 import { Route, Switch } from 'react-router-dom';
 
+import Particles from 'react-particles-js';
+import particles from './particles.json';
+
 import Sidebar from '../Sidebar/Sidebar';
 import Home from '../Home/Home'
 import Cv from '../Cv/Cv';
@@ -14,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.container}>
+        <Particles params={particles} className={styles.particles} />
         <Sidebar />
         <Switch>
           <Route exact path={"/"} component={Home} />
