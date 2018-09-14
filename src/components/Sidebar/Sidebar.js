e91c6dc3bae4ff8d0dad828c0684d8370518efcf
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-09-15 00:23:59 +0200

    Add hamburger menu, Fix responsivity issues

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 
 // Import styles
 import styles from './Sidebar.sass';
@@ -9,12 +9,28 @@ import Navigation from './elements/Navigation/Navigation';
 import Social from '../shared/Social';
 import Logo from './elements/Logo/Logo';
 
-const Sidebar = () => (
-    <aside className={styles.container}>
-        <Logo/>
-        <Navigation/>
-        <Social styles={stylesSocial}/>
-    </aside>
-);
+class Sidebar extends Component {
+    state = {
+        isOpened: false
+    }
+    handleOpen = () => {
+        this.setState(state => ({isOpened: !state.isOpened}))
+    }
+    render() {
+        return(
+            <aside className={`${styles.container} ${this.state.isOpened ? styles.active : ""}`}>
+                <button className={styles.button} onClick={this.handleOpen}>
+                    <div className={styles.b1}></div>
+                    <div className={styles.b2}></div>
+                    <div className={styles.b3}></div>
+                </button>
+                <Logo/>
+                <Navigation/>
+                <Social styles={stylesSocial}/>
+            </aside>
+        );
+    }
+}
+
 
 export default Sidebar;
